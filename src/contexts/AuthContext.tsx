5cebb62c4b86a37252f2740ff388cae3db3662c7
Júlia <julia.pereira@escolaemmovimento.com.br>
2021-06-23 22:09:24 -0300

    FEAT Nova pagina de sala perguntas

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -63,7 +63,6 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
   }
 
     return (
-        
         <AuthContext.Provider value={{ user, signInWithGoogle}}>
             {props.children}
         </AuthContext.Provider>
